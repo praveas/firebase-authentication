d5315a8c8ef43d46c58971a3f0dd4e7ca583658a
Prabesh Pokharel <praveas@gmail.com>
2019-09-18 03:13:30 +1000

    adding files to repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import firebase from "firebase";
+import StyledFirebaseAUth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebase.initializeApp({
+  apiKey: "AIzaSyCfBYrmFDWoMzziVlSFLtKjeeRdR7KFIFE",
+  authDomain: "fir-authenication-8340a.firebaseapp.com"
+});
+
+class App extends Component {
+  state = { isSignedIn: false };
+  uiConfig = {
+    signInflow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user });
+      console.log("user", user);
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img src={firebase.auth().currentUser.photoURL} alt="profile" />
+            <br />
+            <h1>Your details are: {firebase.auth().currentUser.email}</h1>
+            <br />
+            <h1>
+              Your Creation Time is:{" "}
+              {firebase.auth().currentUser.metadata.creationTime}
+            </h1>
+          </span>
+        ) : (
+          <StyledFirebaseAUth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
